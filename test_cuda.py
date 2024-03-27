3655ba498c2ef3ff37dc63f7ea682d850d4b373b
CodeWracker <dev.rodrigofs@gmail.com>
2024-03-27 00:09:50 -0300

    forced the source to the right torch version for cuda 11.8

---
+++ test_cuda.py
@@ -0,0 +1,8 @@
+def test_cuda():
+    import torch
+    cuda_version = torch.version.cuda
+    cuda_available = torch.cuda.is_available()
+    return f"CUDA Version: {cuda_version}, CUDA Available: {cuda_available}"
+
+test_cuda_result = test_cuda()
+print(test_cuda_result)
